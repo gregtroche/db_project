c7b7776c40247bdcca4037a49ba169b96129ed6e
Greg Troche <gt2169@nyu.edu>
2023-04-01 13:34:05 -0400

    read working for one table

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const Schools = require('./Schools');
 const database = require('./database');
-// const db = require('pg');
-// const dbConnection = 'postgres://postgres:postgres@127.0.0.1:5432/postgres';
 const app = express();
 app.set('view engine', 'ejs');
 
@@ -22,24 +20,6 @@ app.get('/schoolnew', (req, res) => {
         res.render('pages/schools', {schools: result})
     })
 })
-
-// This route works but it's not as good
-// app.get('/schoolnewnew', (req, res) => {
-//     const dbClient = new db.Client(dbConnection);
-//     dbClient.connect((err) => {
-//         if(err)
-//             throw err;
-//         const query = 'SELECT * FROM schools';
-//         dbClient.query(query, (err, result) => {
-//             if(err)
-//                 throw err;
-//             else {
-//                 res.render('pages/schools', {schools: result});
-//                 res.end;
-//             }
-//         })
-//     }) 
-// })
 
 app.listen(PORT, () => {
     console.log(`Server Running at: http://localhost:${PORT}/`);
