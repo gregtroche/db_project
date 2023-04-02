87988ef9b4c7baa36399c426fb3eeab4877ac125
Greg Troche <gt2169@nyu.edu>
2023-04-01 21:00:10 -0400

    updated yaml

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
     res.send({ 'message':'Endpoint Working' });
 });
 
-app.get('/school', (req, res) => {
+app.get('/schools', (req, res) => {
     const readAllQuery = 'SELECT * FROM schools';
     const data = database.query(readAllQuery);
     data.then(function(result){
