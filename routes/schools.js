6e6025db1a34192f9916d5193fca22b3b54033ab
Greg Troche <gt2169@nyu.edu>
2023-04-02 10:47:15 -0400

    Create and delete successfully added

--- routes/schools.js
+++ routes/schools.js
@@ -1,13 +1,48 @@
 const   express = require('express'),
         router = express.Router(),
-        database = require('../database');
+        database = require('../database'),
+        bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 router.get('/', (req, res) => {
     const readAllQuery = 'SELECT * FROM schools';
     const data = database.query(readAllQuery);
     data.then(function(result){
         res.render('../views/pages/schools', {schools: result})
-    })
+    });
+})
+
+router.post('/', (req,res) => {
+    if(req.body.submissionType === 'create'){
+        const createQuery = `INSERT INTO schools (name, active, last_updated) VALUES('${req.body.schoolName}', ${req.body.active}, now());`;
+        const submission = database.query(createQuery);
+        submission.then(function(result){
+            console.log('Submission Successful!')
+        });
+
+        const readAllQuery = 'SELECT * FROM schools';
+        const data = database.query(readAllQuery);
+        data.then(function(result){
+        res.render('../views/pages/schools', {schools: result})
+        });
+    }
+    else if(req.body.submissionType === 'delete'){
+        const deleteQuery = `DELETE FROM schools WHERE id=${req.body.schoolId};`;
+        const deleteItem = database.query(deleteQuery);
+        deleteItem.then(function(result){
+            console.log('Row Successfully Deleted!')
+        });
+
+        const readAllQuery = 'SELECT * FROM schools';
+        const data = database.query(readAllQuery);
+        data.then(function(result){
+        res.render('../views/pages/schools', {schools: result})
+        });
+    }
 })
 
 module.exports = router;
