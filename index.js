78b4b2b1be96c60d1c40eb132a58e123d2c612ab
Greg Troche <gt2169@nyu.edu>
2023-04-03 21:35:58 -0400

    added forms, create route and date validation

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const PORT = 8080;
 const   express = require('express'),
         app = express(),
-        schools = require('./routes/schools')
+        schools = require('./routes/schools'),
+        events = require('./routes/events');
 
 app.set('view engine', 'ejs');
 
@@ -10,7 +11,8 @@ app.get('/', (req, res) => {
 });
 
 
-app.use('/schools', schools)
+app.use('/schools', schools);
+app.use('/events', events);
 
 app.listen(PORT, () => {
     console.log(`Server Running at: http://localhost:${PORT}/`);
---
+++ routes/events.js
@@ -0,0 +1,49 @@
+const   express = require('express'),
+        router = express.Router(),
+        database = require('../database'),
+        bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+router.get('/', (req, res) => {
+    const readAllQuery = 'SELECT * FROM schools';
+    const data = database.query(readAllQuery);
+    data.then(function(result){
+        res.render('../views/pages/events', {events: result})
+    });
+})
+
+router.post('/', (req,res) => {
+    if(req.body.submissionType === 'create'){ 
+        const createQuery = `INSERT INTO events (school_id, active, name, ceremony_date, website_open_date, website_close_date, last_updated) VALUES(${req.body.schoolId}, ${req.body.active}, '${req.body.eventName}', '${req.body.ceremonyDate}', '${req.body.websiteOpenDate}', '${req.body.websiteCloseDate}', now());`;
+        const submission = database.query(createQuery);
+        submission.then(function(result){
+            console.log('Submission Successful!')
+        });
+    }
+
+    else if(req.body.submissionType === 'delete'){ 
+        // const deleteQuery = `DELETE FROM schools WHERE id=${req.body.schoolId};`;
+        // const deleteItem = database.query(deleteQuery);
+        // deleteItem.then(function(result){
+        //     console.log('Row Successfully Deleted!')
+        // });
+    }
+
+    else if(req.body.submissionType === 'update'){
+        // const updateQuery = `UPDATE schools SET name='${req.body.schoolName}', active=${req.body.active}, last_updated=now() WHERE id=${req.body.schoolId};`;
+        // const updateItem = database.query(updateQuery);
+        // updateItem.then(function(result){
+        //     console.log('Row Successfully Updated!')
+        // });
+        // console.log(req.body)
+        // console.log(updateQuery)
+    }
+
+    res.redirect('back');
+})
+
+module.exports = router;
