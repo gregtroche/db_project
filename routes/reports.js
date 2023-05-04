4763fa2a3a07191e8951d3d20182b12be8effa62
Greg Troche <gt2169@nyu.edu>
2023-05-04 18:48:45 -0400

    added both reports

--- routes/reports.js
+++ routes/reports.js
@@ -17,14 +17,26 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req,res) => {
-    res.redirect(`${req.body.schoolId}`);
+    if(req.body.buttonReport === 'Accessory Pricing') {
+        res.redirect(`accessory-report/${req.body.schoolId}`);
+    } else if (req.body.buttonReport === 'Days to Graduation') {
+        res.redirect(`important-dates/${req.body.schoolId}`);
+    }
 })
 
-router.get('/:id', (req, res) => {
-    const readAllQuery = `SELECT e.name as event_name,agd.name as accessory_group_name,SUM(ap.price) as total_accessory_price FROM schools as s INNER JOIN events e on s.id = e.school_id INNER JOIN accessory_group_data agd on e.id = agd.event_id INNER JOIN accessory_group_product_data agpd on agd.id = agpd.group_id INNER JOIN accessory_products ap on agpd.product_id = ap.id WHERE e.active = true AND e.active = true AND s.id=${req.params.id} GROUP BY accessory_group_name, event_name, ceremony_date;`
+router.get('/accessory-report/:id', (req, res) => {
+    const readAllQuery = `SELECT s.name as school_name, e.name as event_name,agd.name as accessory_group_name,SUM(ap.price) as total_accessory_price FROM schools as s INNER JOIN events e on s.id = e.school_id INNER JOIN accessory_group_data agd on e.id = agd.event_id INNER JOIN accessory_group_product_data agpd on agd.id = agpd.group_id INNER JOIN accessory_products ap on agpd.product_id = ap.id WHERE e.active = true AND e.active = true AND s.id=${req.params.id} GROUP BY accessory_group_name, event_name, ceremony_date, school_name;`
     const data = database.query(readAllQuery);
     data.then(function(result){
         res.render('../views/pages/school-report', {report: result});
+    })
+})
+
+router.get('/important-dates/:id', (req, res) => {
+    const readAllQuery = `SELECT s.name as school_name, e.name as event_name, website_close_date, days_until(website_close_date) as website_close_time, ceremony_date, days_until(ceremony_date) as days_until_ceremony from events as e inner join schools s on e.school_id = s.id where school_id = ${req.params.id} order by days_until_ceremony;`
+    const data = database.query(readAllQuery);
+    data.then(function(result){
+        res.render('../views/pages/ceremony-report', {report: result});
     })
 })
 
